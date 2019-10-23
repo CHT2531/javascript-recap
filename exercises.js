7e9899246300775ee91c45012a8d8732ae1990a9
Matthew Mantle <M.e.Mantle@hud.ac.uk>
2019-10-23 14:47:49 +0100

    Initial commit

---
+++ exercises.js
@@ -0,0 +1,42 @@
+
+/*
+2.
+a) Using these variables and a console.log() statement output the message 'Hi Fred. Your favourite colour is red. Your favourite website is http://www.hud.ac.uk.
+b) If you used the concatenate operator for (a) re-write the statement using a template literal. If you used a template literal re-write the statement using the concatenate operator.
+*/
+const name = "Fred";
+const colour = "red";
+const url="http://www.hud.ac.uk";
+/*
+3. Have a look at the following variable
+a) Create a third variable, name it total. total should be assigned a value that is the sum of num1 and num2. Using these variables and a console.log() output the value of total e.g. '10 + 20 = 30'
+b) Create another variable, call it average. average should be assigned a value that is the mean average of num1 and $num. Again, use a console.log statement to output the value of average.
+*/
+ const num1=10;
+ const num2=20;
+
+/*
+4. Uncomment following three  variables.
+The variables assign1 and assign2 store the marks out of 100 for a student for two different assignments. Assignment 1 has a weighting of 40%, Assignment 2 has a weighting of 60%. Create another variable called overall. Using the variables assign1 and assign2 calculate an overall mark for the student and assign this value to the variable overall. Use a console log statement output the mark.
+*/
+const assign1 = 56;
+const assign2 = 78;
+
+
+/*
+5.
+a) In order to pass a module students must get an overall mark that is greater or equal to 40. Write an if statement that will test if overall is greater than or equal to 40. If it is, use an console.log statement to output "passed". If it isn't use an console.log statement to output "failed".
+b) Write another if statement. This time it should test the value of the variable overall and output if the student has an A, B, C, D etc.
+*/
+
+/*
+6.
+The Kaboom Gas company charge their customers for gas as follows:
+Units of Gas Used Cost(£)
+Units of Gas:0 to 500 Cost:£10
+Units of Gas:501 to 1000 Cost:£10 + 5p for each unit over 500
+Units of Gas:Over 1000 Cost:£35 + 3p for each unit over 1000
+The following JavaScript code assigns a random number value to the variable units. Uncomment the code and write some additional code that will calculate and output the cost of a gas bill based on the value of units.
+*/
+// const units=Math.ceil(Math.random() * 1500);
+// console.log(`Units has a value of ${units}.`);
