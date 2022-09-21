3305bdf0868f433d2556b1d8903b411556e51faa
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-09-21 14:22:12 +0100

    Update exercises.js

--- js/exercises.js
+++ js/exercises.js
@@ -30,22 +30,7 @@ const filmObj = {
 }
 
 /*
-4. The following code declares an array of objects. 
-a) Using this array, write a line of code that outputs 'The film Jaws was released in 1975' to the console.
-b) Add another film to the array. Use a console.log statement to output the details of the new film.
-c) Use a forEach loop to output the details of each film in turn.
-d) Use a filter to get all the films made in the 21st century. 
-*/
-
-const films=[
-  {title:"Jaws", year:1975, duration:124},
-  {title:"Get Out", year:2017, duration:117},
-  {title:"Winter's Bone", year:2010, duration:100},
-  {title:"The Incredibles", year:2004, duration:115},
-]
-
-/*
-5. Uncomment the following code
+4. Uncomment the following code
 a) First, try and understand what the search() method does. Try changing the value of the variable searchTerm to get different messages.
 b) How could you use the search method in a filter function i.e. it should filter an array of film objects based on whether the film title contains searchTerm. Instead printing a message, the if statement should return true or false, just like the other filter examples.
 */
@@ -60,6 +45,23 @@ if(filmObj.title.search(searchTerm)>-1){
 }
 */
 
+
+/*
+5. The following code declares an array of objects. 
+a) Using this array, write a line of code that outputs 'The film Jaws was released in 1975' to the console.
+b) Add another film to the array. Use a console.log statement to output the details of the new film.
+c) Use a forEach loop to output the details of each film in turn. 
+   If you are looking in the notes, make sure you are looking at arrays of objects i.e. https://github.com/CHT2531/javascript-recap/blob/master/notes.md#objects-and-arrays
+d) Use a filter to get all the films made in the 21st century. 
+*/
+
+const films=[
+  {title:"Jaws", year:1975, duration:124},
+  {title:"Get Out", year:2017, duration:117},
+  {title:"Winter's Bone", year:2010, duration:100},
+  {title:"The Incredibles", year:2004, duration:115},
+]
+
 /*
 6. Look at the following code that declares a function
 a) Write a line of code that will call this function so that the console.log message is displayed
@@ -73,21 +75,29 @@ function printDetails(){
 7. The following all relate to the function you have just created
 a) Modify the function so that you can pass an argument for the title of the film. The function call will then look like the following:
 */
-
 //printDetails("Jaws"); //calls the printDetails function passing a single argument.
 
 //b) Modify the function again so that you can also pass arguments for the year and duration.
 
-//c) Instead of passing separate values for title, year and duration, can you pass a film object? The function call will look like the following:
+//c) Re-write the function as a function expression. Check it still works
+
+//d) Re-write the function as an arrow function. Again, check it still works. 
+
+
+//8.  
+//a) Instead of passing separate values for title, year and duration, can you pass a film object? The function call will look like the following:
 
 //printDetails(filmObj); //calls the printDetails function passing the film object we created earlier (Q3) as an argument.
-
-//d) Instead of passing a single object can you pass an array of film objects. Again we can use the array we declared earlier as part of Q4.
-
-
+ 
+/*
+b)Instead of passing a single object can you pass an array of film objects. 
+  Again we can use the array we declared earlier as part of Q5. 
+  You will need to use a forEach loop inside the function to iterate over the film objects.
+*/
 
 
 /*
+9.
 Uncomment the following code. This function accepts a single argument, a film object.
 The function should test the year property of the object and return true if the film was made in the 21st century and false if it wasn't.
 The code beneath will test your function i.e. you should get a message  of 'It's not a 21st century film' (assuming filmObj is the one we declared for Q3).
@@ -107,7 +117,11 @@ if(is21stCentury(filmObj)){
 */
 
 /*
-Have a look at the following code. It calls a function getLongFilms(). getLongFilms() should accept an array of films. This array of films should be filtered to remove films shorter than 120 minutes in length. The filtered array of films should be returned. If you do this correctly, the code that follows will print out the titles of the matching films. Again this assumes we are using the array of films created as part of Q4.
+10.
+Have a look at the following code. It calls a function getLongFilms(). getLongFilms() should accept an array of films. 
+This array of films should be filtered to remove films shorter than 120 minutes in length. 
+The filtered array of films should be returned. If you do this correctly, the code that follows will print out the titles of the matching films. 
+Again this assumes we are using the array of films created as part of Q5.
 */
 
 /*
@@ -119,12 +133,15 @@ const longFilms = getLongFilms(films);
 console.log("Here are all the films that are longer than 2 hours in length:")
 longFilms.forEach(function(film){
     console.log(`${film.title}`);
-  })
+ })
 */
 
 
 /*
-Have a look at the following code. It calls a function searchFilms(). A search term and an array of film objects are passed as arguments.  Add code in the searchFilms function that will filter the films using the search term and return an array of matching films. If you do this correctly, the code that follows will print out the matching films' details
+11.
+Have a look at the following code. It calls a function searchFilms(). A search term and an array of film objects are passed as arguments.  
+Add code in the searchFilms() function that will filter the films using the search term and return an array of matching films. 
+If you do this correctly, the code that follows will print out the matching films' details
 */
 
 /*
